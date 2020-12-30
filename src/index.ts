19fc1045fe6cc1e4f75b00d125df52dcc9998173
ThGnommy <th.brandoli@gmail.com>
2020-12-30 16:52:39 +0100

    first

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+// api key = c8f3fb8806f350497f7a43ce886f000306bced4a
+// mapbox access token = pk.eyJ1IjoiZ25vbW15cyIsImEiOiJja2pibDFmY2wwbHNoMnVzY2s4cXlrNmtvIn0.0iJX7gXFiTMCqaQHrtwkLA
+// Import styles
+import "../src/reset.css";
+import "../src/style.css";
+import * as L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const mymap = L.map("mapid").setView([51.505, -0.09], 13);
+
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ25vbW15cyIsImEiOiJja2pibDFmY2wwbHNoMnVzY2s4cXlrNmtvIn0.0iJX7gXFiTMCqaQHrtwkLA",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: "your.mapbox.access.token",
+  }
+).addTo(mymap);
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const NodeEnvPlugin = require("node-env-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  devtool: NodeEnvPlugin.devtool,
+  devServer: {
+    open: true,
+    contentBase: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new Dotenv({
+      systemvars: true,
+      silent: true,
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: "/node_modules",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: "file-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
